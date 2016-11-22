db3671f5813dc0b60a4f2bb8939d31c97b879826
Anton Davydov <antondavydov.o@gmail.com>
2016-11-23 00:24:51 +0300

    Display all tasks on index page

--- apps/web/views/tasks/index.rb
+++ apps/web/views/tasks/index.rb
@@ -1,5 +1,9 @@
 module Web::Views::Tasks
   class Index
     include Web::View
+
+    def tasks
+      TaskRepository.new.all
+    end
   end
 end
--- spec/web/views/tasks/index_spec.rb
+++ spec/web/views/tasks/index_spec.rb
@@ -2,11 +2,19 @@ require_relative '../../../../apps/web/views/tasks/index'
 
 RSpec.describe Web::Views::Tasks::Index do
   let(:exposures) { Hash[foo: 'bar'] }
-  let(:template)  { Hanami::View::Template.new('apps/web/templates/tasks/index.html.erb') }
+  let(:template)  { Hanami::View::Template.new('apps/web/templates/tasks/index.html.slim') }
   let(:view)      { described_class.new(template, exposures) }
   let(:rendered)  { view.render }
 
-  it 'exposes #foo' do
-    expect(view.foo).to eq exposures.fetch(:foo)
+  let(:repository) { TaskRepository.new }
+
+  describe '#tasks' do
+    before do
+      3.times { |i| TaskRepository.new.create(title: "title ##{i}") }
+    end
+
+    it 'returns all tasks' do
+      expect(view.tasks).to eq repository.all
+    end
   end
 end
