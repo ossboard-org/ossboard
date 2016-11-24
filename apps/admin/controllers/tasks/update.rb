93277a755beaba9dcaa0977da686cbc95a97a49e
Anton Davydov <antondavydov.o@gmail.com>
2016-11-24 18:55:54 +0300

    Update tasks from admin page

--- apps/admin/controllers/tasks/update.rb
+++ apps/admin/controllers/tasks/update.rb
@@ -1,8 +1,25 @@
 module Admin::Controllers::Tasks
   class Update
     include Admin::Action
+    expose :task
+
+    params do
+      required(:id).filled
+      required(:task).schema do
+        required(:title).filled(:str?)
+        required(:body).filled(:str?)
+      end
+    end
 
     def call(params)
+      @task = TaskRepository.new.find(params[:id])
+
+      if @task && params.valid?
+        @task = TaskRepository.new.update(@task.id, params[:task])
+        redirect_to routes.task_path(task.id)
+      else
+        self.status = 422
+      end
     end
   end
 end
--- spec/admin/controllers/tasks/update_spec.rb
+++ spec/admin/controllers/tasks/update_spec.rb
@@ -1,11 +1,36 @@
-require_relative '../../../../apps/admin/controllers/tasks/update'
+require 'spec_helper'
+require_relative '../../../../apps/web/controllers/tasks/create'
 
 RSpec.describe Admin::Controllers::Tasks::Update do
   let(:action) { described_class.new }
-  let(:params) { Hash[] }
+  let(:repo) { TaskRepository.new }
+  let(:task) { repo.create(title: 'old') }
 
-  it 'is successful' do
-    response = action.call(params)
-    expect(response[0]).to eq 200
+  after { repo.clear }
+
+  describe 'when params valid' do
+    let(:params) { { id: task.id, task: { title: 'test', body: 'long body' } } }
+
+    it { expect(action.call(params)).to redirect_to("/admin/tasks/#{task.id}") }
+
+    it 'updates new task' do
+      action.call(params)
+      task = repo.last
+      expect(task.title).to eq 'test'
+      expect(task.body).to eq 'long body'
+    end
+  end
+
+  describe 'when params invalid' do
+    let(:params) { { id: task.id } }
+
+    it { expect(action.call(params)).to have_http_status(422) }
+
+    it 'does not create new task' do
+      action.call(params)
+      task = repo.last
+      expect(task.title).to eq 'old'
+      expect(task.body).to eq nil
+    end
   end
 end
