8120e0b67e94c84830e5a5f841e09ce3efeb4b1a
Anton Davydov <antondavydov.o@gmail.com>
2016-11-25 20:04:13 +0300

    Use purecss and update navbar

--- apps/web/application.rb
+++ apps/web/application.rb
@@ -234,7 +234,7 @@ module Web
         connect-src 'self';
         img-src 'self' https: data:;
         style-src 'self' 'unsafe-inline' https:;
-        font-src 'self';
+        font-src 'self' 'unsafe-inline' https:;
         object-src 'none';
         plugin-types application/pdf;
         child-src 'self';
