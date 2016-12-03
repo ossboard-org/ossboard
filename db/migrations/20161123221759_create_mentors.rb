bd36efe64997217fbb5cbe4a12fce5838a612fe0
Anton Davydov <antondavydov.o@gmail.com>
2016-12-03 22:42:13 +0300

    Update migration

--- db/migrations/20161123221759_create_mentors.rb
+++ db/migrations/20161123221759_create_mentors.rb
@@ -3,8 +3,11 @@ Hanami::Model.migration do
     create_table :mentors do
       primary_key :id
       column :name,       String
-      column :github,     String
-      column :contacts,   String, text: true
+      column :uid,        String
+      column :login,      String
+      column :email,      String
+      column :avatar_url, String
+      column :bio,        String, text: true
       column :created_at, DateTime
       column :updated_at, DateTime
     end
