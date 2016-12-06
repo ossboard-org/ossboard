eb216f07e11dbd8bca862ec42a3e049308b47199
Anton Davydov <antondavydov.o@gmail.com>
2016-12-06 03:00:52 +0300

    Return user finded by id in user#show

--- apps/admin/controllers/users/show.rb
+++ apps/admin/controllers/users/show.rb
@@ -1,8 +1,10 @@
 module Admin::Controllers::Users
   class Show
     include Admin::Action
+    expose :user
 
     def call(params)
+      @user = UserRepository.new.find(params[:id])
     end
   end
 end
--- spec/admin/controllers/users/show_spec.rb
+++ spec/admin/controllers/users/show_spec.rb
@@ -1,9 +1,22 @@
+require 'spec_helper'
 require_relative '../../../../apps/admin/controllers/users/show'
 
 RSpec.describe Admin::Controllers::Users::Show do
   let(:action) { described_class.new }
+  let(:user) { UserRepository.new.create(name: 'Anton') }
   let(:session) { { current_user: User.new(id: 1, admin: true) } }
-  let(:params)  { { 'rack.session' => session } }
+  let(:params)  { { id: user.id, 'rack.session' => session } }
+
+  after { TaskRepository.new.clear }
 
   it { expect(action.call(params)).to be_success }
+
+  describe 'expose' do
+    context '#user' do
+      it 'returns user by id' do
+        action.call(params)
+        expect(action.user).to eq user
+      end
+    end
+  end
 end
