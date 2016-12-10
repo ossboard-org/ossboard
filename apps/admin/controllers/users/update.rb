2a65601cabb0501bb13cca7e7e44d30e556fd328
Anton Davydov <antondavydov.o@gmail.com>
2016-12-10 03:54:35 +0300

    Admin: add user#update action logic

--- apps/admin/controllers/users/update.rb
+++ apps/admin/controllers/users/update.rb
@@ -1,8 +1,37 @@
 module Admin::Controllers::Users
   class Update
     include Admin::Action
+    expose :user
+
+    params do
+      required(:id).filled
+      required(:user).schema do
+        required(:name).filled(:str?)
+        required(:login).filled(:str?)
+        required(:email).filled(:str?)
+        required(:bio).filled(:str?)
+        required(:admin).filled
+      end
+    end
 
     def call(params)
+      @user = repo.find(params[:id])
+
+      if @user && params.valid?
+        user_params = params[:user]
+        user_params[:admin] = user_params[:admin] == '1'
+        @user = repo.update(@user.id, user_params)
+
+        redirect_to routes.user_path(user.id)
+      else
+        self.status = 422
+      end
+    end
+
+  private
+
+    def repo
+      @repo ||= UserRepository.new
     end
   end
 end
--- spec/admin/controllers/users/update_spec.rb
+++ spec/admin/controllers/users/update_spec.rb
@@ -1,11 +1,39 @@
+require 'spec_helper'
 require_relative '../../../../apps/admin/controllers/users/update'
 
 RSpec.describe Admin::Controllers::Users::Update do
   let(:action) { described_class.new }
-  let(:params) { Hash[] }
+  let(:repo)   { UserRepository.new }
+  let(:user)   { repo.create(name: 'old') }
+  let(:session) { { current_user: User.new(id: 1, admin: true) } }
 
-  it 'is successful' do
-    response = action.call(params)
-    expect(response[0]).to eq 302
+  after { repo.clear }
+
+  describe 'when params valid' do
+    let(:params) { { id: user.id, user: { name: 'test', login: 'test', email: 'test@test.com', bio: 'empty', admin: '1' }, 'rack.session' => session  } }
+
+    it { expect(action.call(params)).to redirect_to("/admin/users/#{user.id}") }
+
+    it 'updates new task' do
+      action.call(params)
+      user = repo.last
+      expect(user.name).to eq 'test'
+      expect(user.login).to eq 'test'
+      expect(user.email).to eq 'test@test.com'
+      expect(user.bio).to eq 'empty'
+      expect(user.admin).to eq true
+    end
+  end
+
+  describe 'when params invalid' do
+    let(:params) { { id: user.id, 'rack.session' => session  } }
+
+    it { expect(action.call(params)).to have_http_status(422) }
+
+    it 'does not create new task' do
+      action.call(params)
+      user = repo.last
+      expect(user.name).to eq 'old'
+    end
   end
 end
