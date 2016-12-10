12cbd9132d387f829ad951c3d362033d300122bb
Anton Davydov <antondavydov.o@gmail.com>
2016-12-10 03:55:43 +0300

    Add edit user button

--- apps/admin/views/users/show.rb
+++ apps/admin/views/users/show.rb
@@ -5,5 +5,9 @@ module Admin::Views::Users
     def users_active?
       true
     end
+
+    def link_to_edit
+      link_to 'Edit user', routes.edit_user_path(user.id), class: 'pure-button pure-button-primary'
+    end
   end
 end
