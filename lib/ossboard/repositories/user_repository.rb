d01f4502e2439900ac565448d319cdb5af4bd9de
Anton Davydov <antondavydov.o@gmail.com>
2016-12-14 02:42:03 +0300

    Add hasmany user->tasks association

--- lib/ossboard/repositories/user_repository.rb
+++ lib/ossboard/repositories/user_repository.rb
@@ -1,5 +1,13 @@
 class UserRepository < Hanami::Repository
+  associations do
+    has_many :tasks
+  end
+
   def find_by_uuid(uuid)
     users.where(uuid: uuid).as(User).first
+  end
+
+  def find_with_tasks(id)
+    aggregate(:tasks).where(id: id).as(User).one
   end
 end
--- spec/ossboard/repositories/user_repository_spec.rb
+++ spec/ossboard/repositories/user_repository_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe UserRepository do
   describe '#find_by_uuid' do
     context 'when user exist with uuid' do
       before { repo.create(uuid: 'test') }
+
       it { expect(repo.find_by_uuid('test')).to be_a User }
       it { expect(repo.find_by_uuid('test').uuid).to eq 'test' }
     end
@@ -13,5 +14,21 @@ RSpec.describe UserRepository do
     context 'when user not exist with uuid' do
       it { expect(repo.find_by_uuid('test2')).to eq nil }
     end
+  end
+
+  describe '#find_with_tasks' do
+    let(:task_repo) { TaskRepository.new }
+
+    let(:user) { repo.create(uuid: 'test') }
+
+    before do
+      task_repo.create(title: 'bad', user_id: user.id )
+      task_repo.create(title: 'good', approved: true)
+    end
+
+    subject { repo.find_with_tasks(user.id) }
+
+    it { expect(subject.tasks).to be_a Array }
+    it { expect(subject.tasks.count).to eq 1 }
   end
 end
