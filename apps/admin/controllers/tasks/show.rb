0c3ab3b0cf42b2933f5818162b375968736afb1a
Anton Davydov <antondavydov.o@gmail.com>
2016-12-14 03:00:23 +0300

    Display task author on admin page

--- apps/admin/controllers/tasks/show.rb
+++ apps/admin/controllers/tasks/show.rb
@@ -1,10 +1,11 @@
 module Admin::Controllers::Tasks
   class Show
     include Admin::Action
-    expose :task
+    expose :task, :author
 
     def call(params)
       @task = TaskRepository.new.find(params[:id])
+      @author = UserRepository.new.find(@task.user_id) || User.new(name: 'Anonymous')
     end
   end
 end
