58304a591d6b761bf0f2c3850e1586c9f01fe44e
Anton Davydov <antondavydov.o@gmail.com>
2016-12-15 03:28:40 +0300

    Update user#show layout

--- apps/web/views/users/show.rb
+++ apps/web/views/users/show.rb
@@ -1,5 +1,13 @@
 module Web::Views::Users
   class Show
     include Web::View
+
+    def link_to_task(task)
+      if task.approved
+        link_to task.title, routes.task_path(id: task.id)
+      else
+        html.span { task.title }
+      end
+    end
   end
 end
--- spec/web/views/users/show_spec.rb
+++ spec/web/views/users/show_spec.rb
@@ -9,4 +9,24 @@ RSpec.describe Web::Views::Users::Show do
   it 'exposes #foo' do
     expect(view.foo).to eq exposures.fetch(:foo)
   end
+
+  describe '#link_to_task' do
+    context 'when task approved' do
+      let(:task) { Task.new(id: 1, title: 'test', approved: true) }
+
+      it 'returns link to special task' do
+        link = view.link_to_task(task)
+        expect(link.to_s).to eq '<a href="/tasks/1">test</a>'
+      end
+    end
+
+    context 'when task not approved' do
+      let(:task) { Task.new(id: 1, title: 'test', approved: false) }
+
+      it 'returns task title' do
+        link = view.link_to_task(task)
+        expect(link.to_s).to eq "<span>\ntest\n</span>"
+      end
+    end
+  end
 end
