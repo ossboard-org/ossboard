9af038b97a3545348413805e6ef2bad65f4942b5
Anton Davydov <antondavydov.o@gmail.com>
2016-12-27 02:31:51 +0300

    Hide block button for blocked users

--- apps/admin/views/users/index.rb
+++ apps/admin/views/users/index.rb
@@ -6,6 +6,10 @@ module Admin::Views::Users
       UserRepository.new.all
     end
 
+    def banned_users
+      @banned_users ||= BlokedUserRepository.new.all
+    end
+
     def link_to_user(user)
       link_to user.name, routes.user_path(user.id)
     end
@@ -19,6 +23,8 @@ module Admin::Views::Users
     end
 
     def link_to_block(user)
+      return if banned_users.include?(user.login)
+
       html.form(action: "/admin/users/#{user.id}", method: "POST") do
         input(type: "hidden", name: "_method",  value: "DELETE")
         input(type: "hidden", name: "login",    value: user.login)
