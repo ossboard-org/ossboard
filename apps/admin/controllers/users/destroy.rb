6b05d6f96f0470a57b08b585a3f73b5339e9615b
Anton Davydov <antondavydov.o@gmail.com>
2017-01-04 03:59:10 +0300

    Don't ban admin users

--- apps/admin/controllers/users/destroy.rb
+++ apps/admin/controllers/users/destroy.rb
@@ -3,11 +3,15 @@ module Admin::Controllers::Users
     include Admin::Action
 
     def call(params)
-      BlokedUserRepository.new.create(params[:login])
+      BlokedUserRepository.new.create(params[:login]) unless admin_user?
       redirect_to '/admin/users'
     end
 
   private
+
+    def admin_user?
+      UserRepository.new.find_by_login(params[:login]).admin
+    end
 
     def verify_csrf_token?
       false
--- spec/admin/controllers/users/destroy_spec.rb
+++ spec/admin/controllers/users/destroy_spec.rb
@@ -6,12 +6,26 @@ RSpec.describe Admin::Controllers::Users::Destroy do
   let(:params)  { { id: 1, 'rack.session' => session, login: 'davydovanton' } }
 
   before { Fabricate.create(:user, login: 'davydovanton') }
-  after { REDIS.with(&:flushdb) }
+
+  after do
+    REDIS.with(&:flushdb)
+    UserRepository.new.clear
+  end
 
   it { expect(action.call(params)).to redirect_to '/admin/users' }
 
   it 'blocks user' do
     expect{ action.call(params) }.to change { BlokedUserRepository.new.all.count }.by(1)
     expect(BlokedUserRepository.new.exist?('davydovanton')).to eq true
+  end
+
+  context 'when user is admin' do
+    before { Fabricate.create(:user, login: 'admindavydovanton', admin: true) }
+    let(:params)  { { id: 1, 'rack.session' => session, login: 'admindavydovanton' } }
+
+    it 'does not block user' do
+      expect{ action.call(params) }.to change { BlokedUserRepository.new.all.count }.by(0)
+      expect(BlokedUserRepository.new.exist?('admindavydovanton')).to eq false
+    end
   end
 end
