727f9f279b93b2107670408b7f604feaec727bca
Anton Davydov <antondavydov.o@gmail.com>
2017-01-12 01:31:45 +0300

    Cache AnalyticReporter service

--- lib/ossboard/services/analytic_reporter.rb
+++ lib/ossboard/services/analytic_reporter.rb
@@ -1,9 +1,4 @@
 class AnalyticReporter
-  def initializer
-    @user_repo = UserRepository.new
-    @task_repo = TaskRepository.new
-  end
-
   def call
     {
       labels: last_month_list.map(&:to_s),
@@ -20,31 +15,35 @@ class AnalyticReporter
   private
 
   def users_by_days
-    UserRepository.new.all_from_date(last_month_list.first).group_by{ |u| u.created_at.to_date }
+    @users_by_days ||= UserRepository.new.all_from_date(ONE_MONTH_AGO).group_by{ |u| u.created_at.to_date }
   end
 
   def closed_tasks_by_day
-    TaskRepository.new.all_from_date(last_month_list.first, Task::VALID_STATUSES[:closed])
+    @closed_tasks_by_day ||= task_repo.all_from_date(ONE_MONTH_AGO, Task::VALID_STATUSES[:closed])
       .group_by{ |task| task.created_at.to_date }
   end
 
   def assigned_tasks_by_day
-    TaskRepository.new.all_from_date(last_month_list.first, Task::VALID_STATUSES[:assigned])
+    @assigned_tasks_by_day ||= task_repo.all_from_date(ONE_MONTH_AGO, Task::VALID_STATUSES[:assigned])
       .group_by{ |task| task.created_at.to_date }
   end
 
   def in_progress_tasks_by_day
-    TaskRepository.new.all_from_date(last_month_list.first, Task::VALID_STATUSES[:in_progress])
+    @in_progress_tasks_by_day ||= task_repo.all_from_date(ONE_MONTH_AGO, Task::VALID_STATUSES[:in_progress])
       .group_by{ |task| task.created_at.to_date }
   end
 
   def complited_tasks_by_day
-    TaskRepository.new.all_from_date(last_month_list.first, Task::VALID_STATUSES[:done])
+    @complited_tasks_by_day ||= task_repo.all_from_date(ONE_MONTH_AGO, Task::VALID_STATUSES[:done])
       .group_by{ |task| task.created_at.to_date }
   end
 
   def last_month_list
     @last_month_list ||= (ONE_MONTH_AGO..Date.today)
+  end
+
+  def task_repo
+    @task_repo ||= TaskRepository.new
   end
 
   ONE_MONTH_AGO = Date.today - 30
