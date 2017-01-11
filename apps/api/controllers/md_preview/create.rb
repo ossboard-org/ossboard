726de8682cd10c3b3809bc2348ff2496d7d58669
Anton Davydov <antondavydov.o@gmail.com>
2017-01-11 03:12:09 +0300

    Create simple logic for md preview action

--- apps/api/controllers/md_preview/create.rb
+++ apps/api/controllers/md_preview/create.rb
@@ -2,7 +2,24 @@ module Api::Controllers::MdPreview
   class Create
     include Api::Action
 
+    params do
+      required(:md_text).filled(:str?)
+    end
+
     def call(params)
+      self.body = JSON.generate(text: text)
+    end
+
+  private
+
+    EMPTY_STRING = ''.freeze
+
+    def text
+      if params.valid?
+        params[:md_text]
+      else
+        EMPTY_STRING
+      end
     end
   end
 end
--- spec/api/controllers/md_preview/create_spec.rb
+++ spec/api/controllers/md_preview/create_spec.rb
@@ -4,8 +4,15 @@ RSpec.describe Api::Controllers::MdPreview::Create do
   let(:action) { described_class.new }
   let(:params) { Hash[] }
 
-  it 'is successful' do
-    response = action.call(params)
-    expect(response[0]).to eq 200
+  # expect(task.md_body).to eq 'This is *bongos*, indeed.'
+
+  it { expect(action.call(params)).to be_success }
+  it { expect(action.call(params)).to match_in_body(/\A{"text":""}\z/) }
+
+  context 'when md text is empty' do
+    let(:params) { { md_text: '' } }
+
+    it { expect(action.call(params)).to be_success }
+    it { expect(action.call(params)).to match_in_body(/\A{"text":""}\z/) }
   end
 end
