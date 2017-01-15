d012754f2d53f105c16b787941ff0168ce2c7a20
Anton Davydov <antondavydov.o@gmail.com>
2017-01-16 02:16:11 +0300

    Fix secure headers configuration

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -20,7 +20,4 @@ SecureHeaders::Configuration.default do |config|
     "executionContexts"
   ]
   config.csp = SecureHeaders::OPT_OUT # If this line is omitted, we will assume you meant to opt out.
-  config.csp_report_only = {
-    default_src: %w('self')
-  }
 end
