a6b9240bd2675c56068e33a0706bd2b06a3052ac
Anton Davydov <antondavydov.o@gmail.com>
2017-01-16 03:08:05 +0300

    Fix error with updated_csrf_token

--- apps/web/controllers/tasks/new.rb
+++ apps/web/controllers/tasks/new.rb
@@ -2,7 +2,7 @@ module Web::Controllers::Tasks
   class New
     include Web::Action
 
-    expose :task, :params
+    expose :task, :params, :updated_csrf_token
 
     def call(params)
       @params = params
