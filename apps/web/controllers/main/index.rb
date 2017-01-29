81f316abf787c52452402bebe72a1e0707395981
Anton Davydov <antondavydov.o@gmail.com>
2017-01-29 03:47:35 +0300

    Replace tasks list calculation from view to action

--- apps/web/controllers/main/index.rb
+++ apps/web/controllers/main/index.rb
@@ -1,8 +1,10 @@
 module Web::Controllers::Main
   class Index
     include Web::Action
+    expose :tasks
 
     def call(params)
+      @tasks = TaskRepository.new.only_approved.first(3)
     end
   end
 end
--- spec/web/controllers/main/index_spec.rb
+++ spec/web/controllers/main/index_spec.rb
@@ -5,4 +5,18 @@ RSpec.describe Web::Controllers::Main::Index do
   let(:params) { Hash[] }
 
   it { expect(action.call(params)).to be_success }
+
+  describe '#tasks' do
+    before do
+      10.times { |i| Fabricate.create(:task, title: "title ##{i}", approved: true) }
+      action.call(params)
+    end
+
+    after { TaskRepository.new.clear }
+
+    it { expect(action.tasks.count).to eq 3 }
+    it { expect(action.tasks.last.title).to eq 'title #7' }
+    it { expect(action.tasks.first.title).to  eq 'title #9' }
+  end
+
 end
