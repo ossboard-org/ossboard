0c4f6306e0c00df936c04f918b04ed505e060029
Anton Davydov <antondavydov.o@gmail.com>
2017-02-01 02:55:56 +0300

    Update mail config

--- lib/ossboard.rb
+++ lib/ossboard.rb
@@ -4,5 +4,10 @@ Hanami::Mailer.configure do
   delivery do
     development :test
     test        :test
+    production  :smtp,
+      port:      587,
+      address:   "smtp.mailgun.org",
+      user_name: ENV['MAILGUN_USERNAME'],
+      password:  ENV['MAILGUN_PASSWORD']
   end
 end.load!
