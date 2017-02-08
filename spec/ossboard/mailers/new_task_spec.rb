d6fb29313c25627d363eddc2b915cc1983c5dec2
Anton Davydov <antondavydov.o@gmail.com>
2017-02-08 03:39:29 +0300

    Fix email title spec

--- spec/ossboard/mailers/new_task_spec.rb
+++ spec/ossboard/mailers/new_task_spec.rb
@@ -10,7 +10,7 @@ describe Mailers::NewTask do
     mail = Hanami::Mailer.deliveries.last
 
     expect(mail.to).to eq [user.email]
-    expect(mail.subject).to eq 'OSSBoard: new task was add for moderation'
+    expect(mail.subject).to eq 'OSSBoard: new task'
     expect(mail.body.encoded).to match "http://www.ossboard.org/admin/tasks/#{task.id}"
   end
 end
