11f0f7537cad8b98e5b545990f0c42b5852a6b8f
Anton Davydov <antondavydov.o@gmail.com>
2017-02-15 03:50:20 +0300

    Update hanami to 1.0.0.beta1 :tada:

--- lib/ossboard.rb
+++ lib/ossboard.rb
@@ -1,15 +1,1 @@
 Hanami::Utils.require!("#{__dir__}/ossboard")
-
-Hanami::Mailer.configure do
-  delivery do
-    development :test
-    test        :test
-    production  :smtp,
-      port:      587,
-      address:   "smtp.mailgun.org",
-      user_name: ENV['MAILGUN_USERNAME'],
-      password:  ENV['MAILGUN_PASSWORD'],
-      authentication: 'plain',
-      enable_starttls_auto: true
-  end
-end.load!
