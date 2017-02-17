92532dd181810869bf562476336bc633ade002c7
Anton Davydov <antondavydov.o@gmail.com>
2017-02-17 15:10:43 +0300

    Add attributes to user and task entities

--- lib/ossboard/entities/user.rb
+++ lib/ossboard/entities/user.rb
@@ -1,4 +1,23 @@
 class User < Hanami::Entity
+  attributes do
+    attribute :id,             Types::Int
+    attribute :count,          Types::Int
+    attribute :name,           Types::String
+    attribute :login,          Types::String
+    attribute :email,          Types::String
+    attribute :avatar_url,     Types::String
+    attribute :bio,            Types::String
+    attribute :admin,          Types::Bool
+    attribute :created_at,     Types::Time
+    attribute :updated_at,     Types::Time
+    attribute :created_at_day, Types::Date
+
+    attribute :tasks,    Types::Collection(Task)
+    attribute :points,   Types::Collection(Point)
+    attribute :repos,    Types::Collection(Repo)
+    attribute :accounts, Types::Collection(Account)
+  end
+
   def registred?
     !!id
   end
