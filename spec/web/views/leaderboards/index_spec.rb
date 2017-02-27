cd93a42212c2796a0ba09ae155113c7f094465f9
nhpd <dmitry.klpv@gmail.com>
2017-02-27 23:17:18 +0300

    Add test for leaderboard view #user_information(user) method

--- spec/web/views/leaderboards/index_spec.rb
+++ spec/web/views/leaderboards/index_spec.rb
@@ -3,12 +3,23 @@ require_relative '../../../../apps/web/views/leaderboards/index'
 RSpec.describe Web::Views::Leaderboards::Index do
   let(:exposures) { Hash[foo: 'bar'] }
   let(:template)  { Hanami::View::Template.new('apps/web/templates/leaderboards/index.html.slim') }
+  let(:user)      { User.new(id: 1, admin: false, login: 'whatever', avatar_url: 'test_url') }
   let(:view)      { described_class.new(template, exposures) }
   let(:rendered)  { view.render }
 
   describe 'exposes' do
     describe '#users' do
       it { expect(view.users).to every(be_an(User)) }
+    end
+
+    describe '#user_information' do
+      let(:info) { view.user_information(user) }
+      
+      it { expect(info).to have_css('.user-row__name', count: 1) }
+      it { expect(info).to have_css('.user-row__avatar', count: 1) }
+      it { expect(info).to have_selector(:css, 'a[href="/users/whatever"]') }
+      it { expect(info).to have_selector(:css, 'img[src="test_url"]') }
+      it { expect(info).to have_content(user.login, count: 1) }
     end
   end
 end
