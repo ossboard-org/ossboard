d4a32af7e3f885792a67e2d8142c8dbdaccf047b
Anton Davydov <antondavydov.o@gmail.com>
2017-03-02 00:19:33 +0300

    Simple refactoring for web tasks#create action

--- apps/web/controllers/tasks/create.rb
+++ apps/web/controllers/tasks/create.rb
@@ -19,15 +19,10 @@ module Web::Controllers::Tasks
 
     def call(params)
       if params.valid? && authenticated?
-        task_params = params[:task]
-        task_params[:body] = Markdown.parse(task_params[:md_body])
-        task_params[:status] = Task::VALID_STATUSES[:in_progress]
-        task_params[:approved] = nil
-
         task = TaskRepository.new.create(task_params)
 
         NewTaskNotificationWorker.perform_async(task.id)
-        flash[:info] = 'Task had been added to moderation. You can check your task status on profile page'
+        flash[:info] = INFO_MESSAGE
 
         redirect_to routes.tasks_path
       else
@@ -35,6 +30,18 @@ module Web::Controllers::Tasks
         self.body = Web::Views::Tasks::New.render(format: format, task: @task,
           current_user: current_user, params: params, updated_csrf_token: set_csrf_token)
       end
+    end
+
+    private
+
+    INFO_MESSAGE = 'Task had been added to moderation. You can check your task status on profile page'.freeze
+
+    def task_params
+      hash = params[:task]
+      hash[:body] = Markdown.parse(hash[:md_body])
+      hash[:status] = Task::VALID_STATUSES[:in_progress]
+      hash[:approved] = nil
+      hash
     end
   end
 end
