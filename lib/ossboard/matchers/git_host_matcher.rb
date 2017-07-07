47cd9ca654767de52e0a7336528bba1a1613d068
Anton Davydov <antondavydov.o@gmail.com>
2017-07-07 19:56:52 +0300

    Isolate GitHostMatcher to module

--- lib/ossboard/matchers/git_host_matcher.rb
+++ lib/ossboard/matchers/git_host_matcher.rb
@@ -1,20 +1,24 @@
 require "dry-matcher"
 
-GITHUB_ISSUE_REGEXP = %r[\.com/([\w-]+)/([\w.-]+)/issues/(\d+)].freeze
+module Matchers
+  module GitHost
+    ISSUE_REGEXP = %r[\.com/([\w-]+)/([\w.-]+)/issues/(\d+)].freeze
 
-success_case = Dry::Matcher::Case.new(
-  match:   -> value, pattern do
-    value.match(pattern.to_s) && value.match(GITHUB_ISSUE_REGEXP)
-  end,
-  resolve: -> value do
-    match = value.match(GITHUB_ISSUE_REGEXP)
-    { org: match[1], repo: match[2], issue: match[3] }
+    SUCCESS_CASE = Dry::Matcher::Case.new(
+      match:   -> value, pattern do
+        value.match(pattern.to_s) && value.match(ISSUE_REGEXP)
+      end,
+      resolve: -> value do
+        match = value.match(ISSUE_REGEXP)
+        { org: match[1], repo: match[2], issue: match[3] }
+      end
+    )
+
+    FAILURE_CASE = Dry::Matcher::Case.new(
+      match: -> value { true },
+      resolve: -> _ {}
+    )
+
+    Matcher = Dry::Matcher.new(success: SUCCESS_CASE, failure: FAILURE_CASE)
   end
-)
-
-failure_case = Dry::Matcher::Case.new(
-  match: -> value { true },
-  resolve: -> _ {}
-)
-
-GitHostMatcher = Dry::Matcher.new(success: success_case, failure: failure_case)
+end
