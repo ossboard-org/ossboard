be20a7687ae3fbfdffdd813cfedad07723a62ec8
Anton Davydov <antondavydov.o@gmail.com>
2017-07-14 01:38:18 +0300

    Create container for lib folder

---
+++ lib/container.rb
@@ -0,0 +1,10 @@
+require 'dry-container'
+require 'dry-auto_inject'
+require_relative 'ossboard/core/markdown'
+
+class Container
+  extend Dry::Container::Mixin
+  register('core.markdown', OSSBoard::Markdown.new)
+end
+
+# Import = Dry::AutoInject(Container)
