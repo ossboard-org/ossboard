113cd4c15091c7038c1ef4bbe9ae97b651eb8eef
Anton Davydov <antondavydov.o@gmail.com>
2017-07-16 15:10:49 +0300

    Replace other services to container

--- lib/container.rb
+++ lib/container.rb
@@ -3,9 +3,10 @@ require 'dry-auto_inject'
 
 %w[markdown http_request].each { |file| require_relative "ossboard/core/#{file}" }
 
-%w[base analytic_reporter github_issue_requester gitlab_issue_requester].each do |file|
-  require_relative "ossboard/services/#{file}"
-end
+%w[
+  base analytic_reporter github_issue_requester
+  gitlab_issue_requester points_calculator task_tweeter
+].each { |file| require_relative "ossboard/services/#{file}" }
 
 class Container
   extend Dry::Container::Mixin
@@ -16,6 +17,8 @@ class Container
   register('services.analytic_reporter', Services::AnalyticReporter.new)
   register('services.github_issue_requester', Services::GithubIssueRequester.new)
   register('services.gitlab_issue_requester', Services::GitlabIssueRequester.new)
+  register('services.points_calculator', Services::PointsCalculator.new)
+  register('services.task_twitter', Services::TaskTwitter.new)
 end
 
 Import = Dry::AutoInject(Container)
