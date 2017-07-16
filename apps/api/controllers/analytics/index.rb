d4d61841263091eec51b4b8e2e1be937bcfced0d
Anton Davydov <antondavydov.o@gmail.com>
2017-07-16 14:57:12 +0300

    Add services to container and import class

--- apps/api/controllers/analytics/index.rb
+++ apps/api/controllers/analytics/index.rb
@@ -1,9 +1,10 @@
 module Api::Controllers::Analytics
   class Index
     include Api::Action
+    include Import['services.analytic_reporter']
 
     def call(params)
-      self.body = JSON.generate(Services::AnalyticReporter.new.call)
+      self.body = JSON.generate(analytic_reporter.call)
     end
   end
 end
--- lib/container.rb
+++ lib/container.rb
@@ -3,10 +3,15 @@ require 'dry-auto_inject'
 
 require_relative 'ossboard/core/markdown'
 require_relative 'ossboard/core/http_request'
+require_relative 'ossboard/services/analytic_reporter'
 
 class Container
   extend Dry::Container::Mixin
+
   register('core.markdown', Core::Markdown.new)
   register('core.http_request', Core::HttpRequest)
+
+  register('services.analytic_reporter', Services::AnalyticReporter.new)
 end
+
 Import = Dry::AutoInject(Container)
