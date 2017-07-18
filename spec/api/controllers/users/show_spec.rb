6f2970cd86736946f5fa42c0c83d7df3ec98901b
Anton Davydov <antondavydov.o@gmail.com>
2017-07-18 22:34:24 +0300

    Update specs

--- spec/api/controllers/users/show_spec.rb
+++ spec/api/controllers/users/show_spec.rb
@@ -2,16 +2,27 @@ require_relative '../../../../apps/api/controllers/users/show'
 
 RSpec.describe Api::Controllers::Users::Show do
   let(:action) { described_class.new }
-  let(:params) { Hash[id: 'davydovanton'] }
+  let(:params) { Hash[id: user.login] }
+  let(:user) { Fabricate.create(:user, name: 'Anton', login: 'davydovanton') }
 
   subject { action.call(params) }
+
+  after { UserRepository.new.clear }
 
   context 'when params invalid' do
     let(:params) { Hash[] }
     it { expect(subject).to be_success }
+    it { expect(subject).to match_in_body '{}' }
+  end
+
+  context 'when user not exist params invalid' do
+    let(:params) { Hash[id: 'notexist'] }
+    it { expect(subject).to be_success }
+    it { expect(subject).to match_in_body '{}' }
   end
 
   context 'when params valid' do
     it { expect(subject).to be_success }
+    it { expect(subject).to match_in_body '"name":"Anton","login\":"davydovanton"' }
   end
 end
