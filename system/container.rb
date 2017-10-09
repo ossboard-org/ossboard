3620763ef87fc445835b4980160b69947ddb1676
Anton Davydov <antondavydov.o@gmail.com>
2017-10-10 02:08:10 +0300

    Use custom folder resolver

--- system/container.rb
+++ system/container.rb
@@ -1,17 +1,11 @@
 require 'dry/system/container'
+require_relative 'resolver'
 
 class Container < Dry::System::Container
-  namespace('core') do
-    register('markdown_parser') { load! 'ossboard/core/markdown_parser' }
-    register('http_request') { load! 'ossboard/core/http_request' }
-  end
+  extend Dry::Hanami::Resolver
 
-  namespace('services') do
-    register('analytic_reporter') { load! 'ossboard/services/analytic_reporter' }
-    register('points_calculator') { load! 'ossboard/services/points_calculator' }
-    register('url_shortener')     { load! 'ossboard/services/url_shortener' }
-    register('task_twitter')      { load! 'ossboard/services/task_twitter' }
-  end
+  register_folder! 'ossboard/core'
+  register_folder! 'ossboard/services'
 
   namespace('tasks') do
     namespace('interactors') do
@@ -21,28 +15,12 @@ class Container < Dry::System::Container
       register('issue_information') { Tasks::Interactors::IssueInformation }
     end
 
-    namespace('services') do
-      register('github_issue_requester') { load! 'tasks/services/github_issue_requester' }
-      register('gitlab_issue_requester') { load! 'tasks/services/gitlab_issue_requester' }
-    end
-
     register('matchers.git_host') do
       load_file! 'tasks/matchers/git_host'
       Tasks::Matchers::GitHost::Matcher
     end
   end
+  register_folder! 'tasks/services'
 
-  def self.load_file!(path)
-    require_relative "#{Hanami.root}/lib/#{path}"
-  end
-
-  def self.load!(path)
-    load_file!(path)
-
-    path.sub!('ossboard/', '')
-
-    Object.const_get(Inflecto.classify(path)).new
-  end
-
-  configure 
+  configure
 end
