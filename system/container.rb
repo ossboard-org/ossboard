ea1be054df376d927a48b69059ce670eaebf9572
Anton Davydov <antondavydov.o@gmail.com>
2017-10-10 01:41:43 +0300

    Create some metamagic for load container dependencies

--- system/container.rb
+++ system/container.rb
@@ -2,52 +2,28 @@ require 'dry/system/container'
 
 class Container < Dry::System::Container
   namespace('core') do
-    register('markdown_parser') do
-      load_file! 'ossboard/core/markdown_parser'
-      Core::MarkdownParser.new
-    end
-    register('http_request') do
-      load_file! 'ossboard/core/http_request'
-      Core::HttpRequest.new
-    end
+    register('markdown_parser') { load! 'ossboard/core/markdown_parser' }
+    register('http_request') { load! 'ossboard/core/http_request' }
   end
 
   namespace('services') do
-    register('analytic_reporter') do
-      load_file! 'ossboard/services/analytic_reporter'
-      Services::AnalyticReporter.new
-    end
-    register('points_calculator') do
-      load_file! 'ossboard/services/points_calculator'
-      Services::PointsCalculator.new
-    end
-    register('url_shortener') do
-      load_file! 'ossboard/services/url_shortener'
-      Services::UrlShortener.new
-    end
-    register('task_twitter') do
-      load_file! 'ossboard/services/task_tweeter'
-      Services::TaskTwitter.new
-    end
+    register('analytic_reporter') { load! 'ossboard/services/analytic_reporter' }
+    register('points_calculator') { load! 'ossboard/services/points_calculator' }
+    register('url_shortener')     { load! 'ossboard/services/url_shortener' }
+    register('task_twitter')      { load! 'ossboard/services/task_tweeter' }
   end
 
   namespace('tasks') do
     namespace('interactors') do
-      register('create') { Tasks::Interactors::Create }
-      register('update') { Tasks::Interactors::Update }
-      register('update_status') { Tasks::Interactors::UpdateStatus }
+      register('create')            { Tasks::Interactors::Create }
+      register('update')            { Tasks::Interactors::Update }
+      register('update_status')     { Tasks::Interactors::UpdateStatus }
       register('issue_information') { Tasks::Interactors::IssueInformation }
     end
 
     namespace('services') do
-      register('github_issue_requester') do
-        load_file! 'tasks/services/github_issue_requester'
-        Tasks::Services::GithubIssueRequester.new
-      end
-      register('gitlab_issue_requester') do
-        load_file! 'tasks/services/gitlab_issue_requester'
-        Tasks::Services::GitlabIssueRequester.new
-      end
+      register('github_issue_requester') { load! 'tasks/services/github_issue_requester' }
+      register('gitlab_issue_requester') { load! 'tasks/services/gitlab_issue_requester' }
     end
 
     register('matchers.git_host') do
@@ -57,8 +33,14 @@ class Container < Dry::System::Container
   end
 
   def self.load_file!(path)
-    container_path = File.dirname(__FILE__)
-    require_relative "#{container_path}/../lib/#{path}"
+    require_relative "#{Hanami.root}/lib/#{path}"
+  end
+
+  def self.load!(path)
+    load_file!(path)
+
+    klass = Inflecto.classify(path.sub!('ossboard/', ''))
+    Object.const_get(klass).new
   end
 
   configure
