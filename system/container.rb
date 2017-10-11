8e45e0e65d1dd7ae64eac8dde3c8d22210e20bd8
Anton Davydov <antondavydov.o@gmail.com>
2017-10-12 00:55:02 +0300

    Add ability to use custom resolver

--- system/container.rb
+++ system/container.rb
@@ -7,6 +7,7 @@ class Container < Dry::System::Container
   register_folder! 'ossboard/repositories'
   register_folder! 'ossboard/core'
   register_folder! 'ossboard/services'
+  # register_folder! 'ossboard/workers', resolver: ->(k) { k }
 
   namespace('tasks') do
     namespace('interactors') do
--- system/resolver.rb
+++ system/resolver.rb
@@ -3,10 +3,10 @@ module Dry
     module Resolver
       PROJECT_NAME = ::Hanami::Environment.new.project_name
 
-      def register_folder!(folder)
+      def register_folder!(folder, resolver: ->(k) { k.new })
         all_files_in_folder(folder).each do |file|
           register_name = file.sub("#{PROJECT_NAME}/", '').gsub('/', '.').gsub(/_repository\z/, '')
-          register(register_name, memoize: true) { load! file }
+          register(register_name, memoize: true) { load! file, resolver: resolver }
         end
       end
 
@@ -17,13 +17,13 @@ module Dry
           .map! { |file_name| "#{folder}/#{file_name.sub!('.rb', '')}" }
       end
 
-      def load!(path)
+      def load!(path, resolver: ->(k) { k.new })
         load_file!(path)
 
         unnecessary_part = path[/repositories/] ? "#{PROJECT_NAME}/repositories" : "#{PROJECT_NAME}/"
         right_path = path.sub(unnecessary_part, '')
 
-        Object.const_get(Inflecto.classify(right_path)).new
+        resolver.call(Object.const_get(Inflecto.classify(right_path)))
       end
 
       def load_file!(path)
