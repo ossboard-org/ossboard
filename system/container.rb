50b1a5e0fd2e85a1bb3c6da51f225ed90fa624b9
Anton Davydov <antondavydov.o@gmail.com>
2017-10-12 01:05:52 +0300

    Wrap matchers

--- system/container.rb
+++ system/container.rb
@@ -8,16 +8,11 @@ class Container < Dry::System::Container
   register_folder! 'ossboard/repositories'
   register_folder! 'ossboard/core'
   register_folder! 'ossboard/services'
-  # register_folder! 'ossboard/workers', resolver: ->(k) { k }
 
   # Tasks domain
   register_folder! 'tasks/services'
   register_folder! 'tasks/interactors', resolver: ->(k) { k }
-
-  register('tasks.matchers.git_host') do
-    load_file! 'tasks/matchers/git_host'
-    Tasks::Matchers::GitHost::Matcher
-  end
+  register_folder! 'tasks/matchers', resolver: ->(k) { k::Matcher }
 
   configure
 end
