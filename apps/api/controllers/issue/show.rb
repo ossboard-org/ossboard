4784a55fc211ebe4f01374bab5d56dc9ac5b9dcf
Anton Davydov <antondavydov.o@gmail.com>
2019-02-09 22:47:31 +0300

    Use operation instead hanami interactor object

--- apps/api/controllers/issue/show.rb
+++ apps/api/controllers/issue/show.rb
@@ -1,19 +1,26 @@
+require 'dry/monads/result'
+require 'dry/monads/do'
+
 module Api::Controllers::Issue
   class Show
     include Api::Action
     include Hanami::Serializer::Action
-    include Import['tasks.interactors.issue_information']
+    include Dry::Monads::Result::Mixin
+    include Import[operation: 'tasks.operations.issue_information']
 
     params do
       required(:issue_url).filled(:str?)
     end
 
     def call(params)
-      result = issue_information.new(params.valid?, params).call
+      result = operation.call(params.valid?, params)
 
-      status = result.failure? ? 404 : 200
-
-      send_json(result.response, status: status)
+      case result
+      when Success
+        send_json(result.value!, status: 200)
+      when Failure
+        send_json(result.failure, status: 404)
+      end
     end
   end
 end
