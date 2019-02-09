8c9a0619d15a30a26d14d6f24608b82245e68c1c
Anton Davydov <antondavydov.o@gmail.com>
2019-02-09 23:46:38 +0300

    Update dry-system dependency

--- system/boot/redis.rb
+++ system/boot/redis.rb
@@ -1,8 +1,10 @@
-Container.finalize(:redis) do |container|
-  uri = URI.parse(ENV.fetch('REDISTOGO_URL', ''))
-  redis = ConnectionPool.new(size: 10, timeout: 3) do
-    Redis.new(driver: :hiredis, host: uri.host, port: uri.port, password: uri.password)
-  end
+Container.boot(:redis) do |container|
+  init do
+    uri = URI.parse(ENV.fetch('REDISTOGO_URL', ''))
+    redis = ConnectionPool.new(size: 10, timeout: 3) do
+      Redis.new(driver: :hiredis, host: uri.host, port: uri.port, password: uri.password)
+    end
 
-  container.register(:redis, redis)
+    container.register(:redis, redis)
+  end
 end
--- system/boot/twitter.rb
+++ system/boot/twitter.rb
@@ -1,10 +1,12 @@
-Container.finalize(:twitter) do |container|
-  client = Twitter::REST::Client.new do |config|
-    config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
-    config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
-    config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-    config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
-  end
+Container.boot(:twitter) do |container|
+  init do
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+      config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+      config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
+    end
 
-  container.register(:twitter, client)
+    container.register(:twitter, client)
+  end
 end
