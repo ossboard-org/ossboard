bd10cf8658f78ff7d8e9be8491dd42f2cd9498b9
Anton Davydov <antondavydov.o@gmail.com>
2019-02-17 17:30:06 +0300

    Use operation for getting task instead direct repository call

--- apps/web/controllers/tasks/edit.rb
+++ apps/web/controllers/tasks/edit.rb
@@ -1,17 +1,30 @@
+require 'dry/monads/result'
+
 module Web::Controllers::Tasks
   class Edit
     include Web::Action
+    include Dry::Monads::Result::Mixin
+    include Import[operation: 'tasks.operations.show']
+
     expose :task, :params
 
     def call(params)
-      @task = TaskRepository.new.find(params[:id])
+      result = operation.call(id: params[:id])
 
-      unless current_user.can_edit_task?(task)
-        flash[:error] = "You doesn't have access for editing this task"
-        redirect_to routes.task_path(params[:id])
+      case result
+      when Success
+        @task = TaskRepository.new.find(params[:id])
+        @author = UserRepository.new.find(@task.user_id) || User.new(name: 'Anonymous')
+
+        unless current_user.can_edit_task?(@task)
+          flash[:error] = "You doesn't have access for editing this task"
+          redirect_to routes.task_path(params[:id])
+        end
+
+        @params = params
+      when Failure
+        redirect_to(routes.tasks_path)
       end
-
-      @params = params
     end
   end
 end
